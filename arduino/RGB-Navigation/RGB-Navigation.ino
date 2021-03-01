61d081f882c50a7753e76a91f37a039febb02c21
Stijn Rogiest <reddusted@gmail.com>
2021-03-01 21:17:35 +0100

    FastLED

--- arduino/RGB-Navigation/RGB-Navigation.ino
+++ arduino/RGB-Navigation/RGB-Navigation.ino
@@ -1,4 +1,8 @@
+#include <FastLED.h>
+
 #define MAX_LINES 32
+#define LED_COUNT 32
+#define DATA_PIN 3
 
 struct Color
 {
@@ -7,6 +11,7 @@ struct Color
     Color(uint8_t r, uint8_t g, uint8_t b) : r(r), g(g), b(b) {}
 };
 
+// A route
 struct LineEffect
 {
     uint16_t startLed;
@@ -16,7 +21,17 @@ struct LineEffect
     LineEffect(uint16_t startLed, uint16_t endLed, Color color) : startLed(startLed), endLed(endLed), color(color) {}
 };
 
-LineEffect *lines[MAX_LINES];
+// Effect that is used when there are no routes currently displayed
+enum IdleEffect
+{
+    Black,
+    Rainbow
+};
+
+IdleEffect idleEffect = Black;
+LineEffect *routes[MAX_LINES];
+
+CRGB leds[LED_COUNT];
 uint64_t counter = 0;
 
 void setup()
@@ -26,17 +41,84 @@ void setup()
     delay(100);
 
     pinMode(LED_BUILTIN, OUTPUT);
-    memset(lines, 0, sizeof(LineEffect *) * MAX_LINES);
+    memset(routes, 0, sizeof(LineEffect *) * MAX_LINES);
+
+    FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, LED_COUNT);
+    FastLED.setBrightness(40);
 
     for (int i = 0; i < 5; i++)
     {
         digitalWrite(LED_BUILTIN, true);
-        delay(100);
+        delay(50);
         digitalWrite(LED_BUILTIN, false);
-        delay(100);
+        delay(50);
     }
 
     Serial.println("Ready");
+}
+
+void processPacket(int packetType)
+{
+    // Switch does not work for some reason??
+    if (packetType == 1) // Set effect
+    {
+        idleEffect = (IdleEffect)Serial.read();
+        Serial.print("Set idle effect ");
+        Serial.println(idleEffect);
+    }
+    else if (packetType == 2 || packetType == 3) // Enable/disable line
+    {
+        uint8_t id = Serial.read();
+        if (id >= MAX_LINES)
+        {
+            Serial.println("Reached max lines");
+            return;
+        }
+
+        if (packetType == 2)
+        {
+            if (routes[id] != nullptr)
+                delete routes[id];
+
+            // Enable line effect
+            uint8_t r = Serial.read(), g = Serial.read(), b = Serial.read();
+            int start = Serial.read() << 8 | Serial.read();
+            int end = Serial.read() << 8 | Serial.read();
+            routes[id] = new LineEffect(start, end, Color(r, g, b));
+
+            Serial.print("Enable line ");
+            Serial.print(id);
+            Serial.print(", start=");
+            Serial.print(start);
+            Serial.print(", end=");
+            Serial.println(end);
+        }
+        else
+        {
+            // Disable line effect
+            if (routes[id] != nullptr)
+            {
+                delete routes[id];
+                routes[id] = nullptr;
+            }
+
+            Serial.print("Disable line ");
+            Serial.println(id);
+        }
+    }
+    else
+    {
+        Serial.print("Unknown packet type ");
+        Serial.println(packetType);
+    }
+}
+
+void rainbowEffect()
+{
+    for (int i = 0; i < LED_COUNT; i++)
+    {
+        leds[i] = CHSV(((counter / 8) + i) % 255, 255, 255);
+    }
 }
 
 void loop()
@@ -59,68 +141,34 @@ void loop()
         if ((packetType == 2 && available >= 9) || (packetType == 1 && available >= 2) || (packetType == 3 && available >= 2))
         {
             Serial.read(); // Consume packetType
-            Serial.print("Received packet of type ");
-            Serial.println(packetType);
-
-            if (packetType == 1)
-            {
-                // Effect
-                uint8_t effectType = Serial.read();
-                Serial.print("Unimplemented effectType ");
-                Serial.println(effectType);
-            }
-            else if (packetType == 2 || packetType == 3)
-            {
-                uint8_t id = Serial.read();
-                // if (id >= MAX_LINES)
-                // {
-                //     Serial.println("Reached max lines");
-                //     break;
-                // }
-
-                if (packetType == 2)
-                {
-                    if (lines[id] != nullptr)
-                        delete lines[id];
-
-                    // Enable line effect
-                    uint8_t r = Serial.read(), g = Serial.read(), b = Serial.read();
-                    int start = Serial.read() << 8 | Serial.read();
-                    int end = Serial.read() << 8 | Serial.read();
-                    lines[id] = new LineEffect(start, end, Color(r, g, b));
-
-                    Serial.print("Enable line ");
-                    Serial.println(id);
-                }
-                else
-                {
-                    // Disable line effect
-                    if (lines[id] != nullptr)
-                    {
-                        delete lines[id];
-                        lines[id] = nullptr;
-                    }
-
-                    Serial.print("Disable line ");
-                    Serial.println(id);
-                }
-            }
-            else
-            {
-                Serial.print("Unknown packet type ");
-                Serial.println(packetType);
-            }
+            processPacket(packetType);
         }
     }
 
+    bool anyRoute = false;
     for (int i = 0; i < MAX_LINES; i++)
     {
-        LineEffect *le = lines[i];
+        LineEffect *le = routes[i];
         if (!le)
             continue;
+        anyRoute = true;
 
-        digitalWrite(LED_BUILTIN, counter % 1000 < 50);
+        // Draw line effect
+        int dir = le->endLed > le->startLed ? 1 : -1;
+        for (int i = le->startLed; i != le->endLed && i < LED_COUNT && i >= 0; i += dir)
+        {
+            leds[i] = CRGB(le->color.r, le->color.g, le->color.b);
+        }
     }
+
+    if (!anyRoute)
+    {
+        // Draw idle effect
+        if (idleEffect == Rainbow)
+            rainbowEffect();
+    }
+
+    FastLED.show();
 
     counter++;
 }
