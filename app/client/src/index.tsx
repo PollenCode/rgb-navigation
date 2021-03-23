dffe5c074467fb41f661aa0751bbbdf35efdd822
Stijn Rogiest <reddusted@gmail.com>
2021-03-23 10:44:12 +0100

    Remove register page as it is not needed

--- app/client/src/index.tsx
+++ app/client/src/index.tsx
@@ -5,7 +5,6 @@ import { App } from "./App";
 import { SocketContext } from "./socketContext";
 import { io } from "socket.io-client";
 import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
-import { Register } from "./Register";
 
 let socket = io(process.env.NODE_ENV === "development" ? "http://localhost:3001/" : "/");
 
@@ -15,7 +14,6 @@ ReactDOM.render(
             <Router>
                 <Switch>
                     <Route path="/" exact component={App} />
-                    <Route path="/register" component={Register} />
                     <Redirect to="/" />
                 </Switch>
             </Router>
