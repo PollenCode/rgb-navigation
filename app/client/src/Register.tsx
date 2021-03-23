fb6395fbcd031e04c89906219106501d45f347ba
Stijn Rogiest <reddusted@gmail.com>
2021-03-23 10:35:33 +0100

    Register page

---
+++ app/client/src/Register.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Register() {
+    return <div>hello this is the register page</div>;
+}
--- app/client/src/index.tsx
+++ app/client/src/index.tsx
@@ -4,13 +4,21 @@ import "./index.css";
 import { App } from "./App";
 import { SocketContext } from "./socketContext";
 import { io } from "socket.io-client";
+import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import { Register } from "./Register";
 
 let socket = io(process.env.NODE_ENV === "development" ? "http://localhost:3001/" : "/");
 
 ReactDOM.render(
     <React.StrictMode>
         <SocketContext.Provider value={{ socket }}>
-            <App />
+            <Router>
+                <Switch>
+                    <Route path="/" exact component={App} />
+                    <Route path="/register" component={Register} />
+                    <Redirect to="/" />
+                </Switch>
+            </Router>
         </SocketContext.Provider>
     </React.StrictMode>,
     document.getElementById("root")
