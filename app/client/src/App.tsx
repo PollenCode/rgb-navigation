6fd7f193690ba0d18b819b032d502ddd321c8aff
Stijn Rogiest <reddusted@gmail.com>
2021-03-23 10:28:22 +0100

    Use socket.io and styled-components

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -1,39 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { SERVER } from "./constants";
 import { LedControllerServerMessage } from "../../shared/Message";
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+`;
 
 export function App() {
-    const [message, setMessage] = useState<string | null>(null);
-
-    async function refreshMessage() {
-        setMessage(null);
-        try {
-            let response = await fetch(SERVER + "/message", { method: "POST" });
-            let obj = await response.json();
-            setMessage(obj.message);
-        } catch (ex) {
-            setMessage(`Error: could not reach server. Is it turned on? See /app/server for instructions. (${ex})`);
-        }
-    }
-
-    useEffect(() => {
-        refreshMessage();
-    }, []);
-
     return (
-        <div>
-            <h2>This is a react application</h2>
-            <small>Open the network tab to see how the message got fetched!</small>
-            <p>
-                Message from server: <strong>{message ?? "Loading..."}</strong>
-            </p>
-            <button
-                onClick={() => {
-                    refreshMessage();
-                }}
-            >
-                Refresh message 2
-            </button>
+        <Container>
             <button
                 type="button"
                 onClick={async () => {
@@ -51,10 +30,9 @@ export function App() {
                         headers: { "Content-Type": "application/json" },
                         body: JSON.stringify(req),
                     });
-                }}
-            >
+                }}>
                 Set color
             </button>
-        </div>
+        </Container>
     );
 }
--- app/client/src/index.tsx
+++ app/client/src/index.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./App";
+import { SocketContext } from "./socketContext";
+import { io } from "socket.io-client";
+
+let socket = io(process.env.NODE_ENV === "development" ? "http://localhost:3001/" : "/");
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <SocketContext.Provider value={{ socket }}>
+            <App />
+        </SocketContext.Provider>
     </React.StrictMode>,
     document.getElementById("root")
 );
